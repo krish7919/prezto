7cc90db6856ae9dcdabdbca6aed0ac3da8af49fe
Krish <kriiyer@cisco.com>
2020-01-26 20:35:07 +0000

    My customisations - 3

--- modules/history/init.zsh
+++ modules/history/init.zsh
@@ -12,7 +12,6 @@
 
 setopt BANG_HIST                 # Treat the '!' character specially during expansion.
 setopt EXTENDED_HISTORY          # Write the history file in the ':start:elapsed;command' format.
-setopt SHARE_HISTORY             # Share history between all sessions.
 setopt HIST_EXPIRE_DUPS_FIRST    # Expire a duplicate event first when trimming history.
 setopt HIST_IGNORE_DUPS          # Do not record an event that was just recorded again.
 setopt HIST_IGNORE_ALL_DUPS      # Delete an old recorded event if a new event is a duplicate.
@@ -21,14 +20,17 @@ setopt HIST_IGNORE_SPACE         # Do not record an event starting with a space.
 setopt HIST_SAVE_NO_DUPS         # Do not write a duplicate event to the history file.
 setopt HIST_VERIFY               # Do not execute immediately upon history expansion.
 setopt HIST_BEEP                 # Beep when accessing non-existent history.
+setopt HIST_REDUCE_BLANKS # remove superfluous blanks before recording entry.
+#setopt SHARE_HISTORY             # Share history between all sessions.
+#setopt INC_APPEND_HISTORY # write to the history file immediately, not when the shell exits.
 
 #
 # Variables
 #
 
-HISTFILE="${HISTFILE:-${ZDOTDIR:-$HOME}/.zhistory}"  # The path to the history file.
-HISTSIZE=10000                   # The maximum number of events to save in the internal history.
-SAVEHIST=10000                   # The maximum number of events to save in the history file.
+HISTFILE="${HISTFILE:-${ZDOTDIR:-$HOME}/.zsh_history}"  # The path to the history file.
+HISTSIZE=30000                   # The maximum number of events to save in the internal history.
+SAVEHIST=100000                   # The maximum number of events to save in the history file.
 
 #
 # Aliases
